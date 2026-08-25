eb59f8c8f633b386865f0a62abda24ffc58b96b8
boubacaryarra2-collab <boubacaryarra2@icloud.com>
2026-08-25 01:03:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ function ouvrirPanier(){document.getElementById('popup').style.display='block'}
 function fermerPanier(){document.getElementById('popup').style.display='none'}
 function ouvrirFrais(){document.getElementById('popup-frais').style.display='block'}
 function fermerFrais(){document.getElementById('popup-frais').style.display='none'}
+function scrollProduits(){document.getElementById('produits').scrollIntoView({behavior:'smooth'})}
