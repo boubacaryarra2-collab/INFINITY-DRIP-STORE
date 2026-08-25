0566af2f966907070bb3d7cf2ac1f58ecb383e3f
boubacaryarra2-collab <boubacaryarra2@icloud.com>
2026-08-25 00:51:32 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function ouvrirPanier(){document.getElementById('popup').style.display='block'}
+function fermerPanier(){document.getElementById('popup').style.display='none'}
+function ouvrirFrais(){document.getElementById('popup-frais').style.display='block'}
+function fermerFrais(){document.getElementById('popup-frais').style.display='none'}
